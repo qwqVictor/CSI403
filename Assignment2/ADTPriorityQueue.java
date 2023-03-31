a53d5d23e34ba56c450da183efc1109d5dd8b577
qwqVictor <i@qwq.ren>
2023-03-31 16:18:32 +0800

    feat(as2): pq

--- Assignment2/ADTPriorityQueue.java
+++ Assignment2/ADTPriorityQueue.java
@@ -1,21 +1,41 @@
-import java.util.Comparator;
-
 /**
  * Priority queue class implementation
  * 
  * @author Huang Kaisheng <2020215138@stu.cqupt.edu.cn>
  * @version 1.0
  */
+
+import java.util.Comparator;
+import java.util.List;
+
 public class ADTPriorityQueue<T extends Comparable<T>> {
     private ADTHeap<T> heap;
 
-    public MyPriorityQueue() {
+    public ADTPriorityQueue() {
         this.heap = new ADTHeap<T>();
     }
 
-    public MyPriorityQueue(Comparator<T> comparator) {
+    public ADTPriorityQueue(Comparator<T> comparator) {
         this.heap = new ADTHeap<T>(comparator);
     }
 
-    
+    public void insert(T value) {
+        this.heap.insert(value);
+    }
+
+    public T delete() {
+        return this.heap.delete();
+    }
+
+    public T top() {
+        return this.heap.top();
+    }
+
+    public boolean isEmpty() {
+        return this.heap.isEmpty();
+    }
+
+    public void sort(List<T> list) {
+        this.heap.sort(list);
+    }
 }
