ab0407ca1a52c6b97573492aed35278653c3a00f
qwqVictor <i@qwq.ren>
2023-03-31 08:37:01 +0800

    feat(as2): basic structure of heapnode

---
+++ Assignment2/ADTHeap.java
@@ -0,0 +1,8 @@
+/**
+* Heap class implementation
+* @author Huang Kaisheng <2020215138@stu.cqupt.edu.cn>
+* @version 1.0
+*/
+public class ADTHeap<T extends Comparable<T>> {
+    private ADTHeapNode<T> root;
+}
---
+++ Assignment2/ADTHeapNode.java
@@ -0,0 +1,40 @@
+/**
+* Heap node class implementation
+* @author Huang Kaisheng <2020215138@stu.cqupt.edu.cn>
+* @version 1.0
+*/
+public class ADTHeapNode<T> {
+    private T value;
+    private ADTHeapNode<T> left, right;
+
+    ADTHeapNode(T value) {
+        this(value, null, null);
+    }
+
+    ADTHeapNode(T value, ADTHeapNode<T> left, ADTHeapNode<T> right) {
+        this.value = value;
+        this.left = left;
+        this.right = right;
+    }
+
+    /**
+     * @return the value
+     */
+    public T getValue() {
+        return value;
+    }
+
+    /**
+     * @return the left
+     */
+    public ADTHeapNode<T> getLeft() {
+        return left;
+    }
+
+    /**
+     * @return the right
+     */
+    public ADTHeapNode<T> getRight() {
+        return right;
+    }
+}
